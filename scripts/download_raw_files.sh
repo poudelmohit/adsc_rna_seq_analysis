19ff6f1433b618e68390c6bb71419c8b0e2b3096
poudelmohit <poudelmohit59@gmail.com>
2024-10-18 16:09:44 -0400

    script to download raw reads

--- scripts/download_raw_files.sh
+++ scripts/download_raw_files.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=sra_download         # Job name
-#SBATCH --output=sra_download.out       # Standard output
-#SBATCH --error=sra_download.err        # Standard error
-#SBATCH --time=02:00:00                 # Time limit (adjust as needed)
-#SBATCH --partition=debug              # Partition/queue name
-#SBATCH --nodes=1                       # Number of nodes
-#SBATCH --ntasks=1                      # Number of tasks (one process)
-#SBATCH --cpus-per-task=4               # Number of CPU cores (adjust based on resources)
-#SBATCH --mem=16G                       # Memory per node (adjust based on needs)
-#SBATCH --mail-type=ALL                 # Send email on start, end, fail
-#SBATCH --mail-user=poudelmohit59@gmail.com  # Email for notifications
+#SBATCH --job-name=fastq-dump-job       # Job name
+#SBATCH --output=fastq-dump-%j.out      # Output file (%j will be replaced by job ID)
+#SBATCH --error=fastq-dump-%j.err       # Error file (%j will be replaced by job ID)
+#SBATCH --time=2:00:00                 # Time limit (HH:MM:SS)
+#SBATCH --cpus-per-task=10               # Number of CPU cores per task
+#SBATCH --mem=16G                       # Memory limit
+#SBATCH --mail-type=END,FAIL            # Notifications for job done or fail
+#SBATCH --mail-user=poudelmohit59@gmail.com   # Email for notifications
+#SBATCH --partition=debug             # Partition (queue) to submit to
 
-# Load necessary modules (if applicable)
-# Example: module load sratoolkit
+# Source bashrc to ensure PATH is correctly set
+source ~/.bashrc                # Example: Load SRA Toolkit if it's available on your HPC
 
-# Step 5: Download FASTQ files using fastq-dump
-# Use xargs to download each SRR ID in the list
-mkdir -p ../data/raw_reads && cd $_
-cat ../srr_ids_list.txt | xargs -n 1 ../../tools/sratoolkit.*/bin/fastq-dump --split-files --gzip
+echo "starting job.."
+echo $pwd
 
-# Check if the files were downloaded successfully
-ls -lh
+# fastq-dump command
+cat ../srr_ids_list.txt | xargs -n 1 ~/extra/adsc_rna_seq_analysis/tools/sratoolkit.3.1.1-ubuntu64/bin/fastq-dump --split-files --gzip --skip-technical
+
+echo "done"
+
